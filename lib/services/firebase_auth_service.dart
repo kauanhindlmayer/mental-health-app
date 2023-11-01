8d7eefed305ea4b8ad1430e509c41d2f3a02a685
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-11-01 20:07:39 -0300

    feat: implementing new auth providers

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: avoid_print
-
+import 'package:flutter/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:github_sign_in/github_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class FirebaseAuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -29,5 +30,28 @@ class FirebaseAuthService {
 
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
+  Future<UserCredential> signInWithGitHub(BuildContext context) async {
+    final GitHubSignIn gitHubSignIn = GitHubSignIn(
+        clientId: 'clientId',
+        clientSecret: 'clientSecret',
+        redirectUrl: 'https://mentalhealthapp.firebaseapp.com/__/auth/handler');
+
+    final result = await gitHubSignIn.signIn(context);
+
+    final githubAuthCredential = GithubAuthProvider.credential(result.token!);
+
+    return await FirebaseAuth.instance
+        .signInWithCredential(githubAuthCredential);
   }
 }
